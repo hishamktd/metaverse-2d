1c5f4759892d019a7c7ea56772753ab71c0be60d
Hisham <hishammuhammed.in@gmail.com>
2025-01-11 11:55:42 +0530

    cors add

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes/v1";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: false,
+  })
+);
 
 app.use(express.json());
 
 app.use("/api/v1", router);
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
